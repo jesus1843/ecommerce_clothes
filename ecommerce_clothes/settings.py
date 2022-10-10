d9177112c1210615f710b2eaf30c70bc2b13edb5
J Jesús Villanueva Jr <jesus1843@hotmail.com>
2022-10-10 16:11:18 -0500

    Accounts App Created, and CustomUser Model - 2

--- ecommerce_clothes/settings.py
+++ ecommerce_clothes/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "ecommerce_clothes.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [ BASE_DIR / "templates" ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
